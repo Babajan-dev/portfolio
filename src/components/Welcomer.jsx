e6ff43d1471e431a16dcafc0332e0e1125dbdb41
Babajan <soacewoo@gmail.com>
2023-12-08 10:23:45 +0500

    First comment

---
+++ src/components/Welcomer.jsx
@@ -0,0 +1,12 @@
+const Welcomer = () => {
+  return (
+    <section>
+      <header>UX/UI DESIGNER, DEVELOPER</header>
+      <div>data</div>
+      <h1>creative DESICNGER & DEVELOPER</h1>
+      <h2>I AM TTOTOOTOT</h2>
+      <button>contact me</button>
+    </section>
+  );
+};
+export default Welcomer;
