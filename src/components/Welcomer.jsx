e1bcbf950722c2b7460a42289f05ef8c6ffdae45
Babajan <soacewoo@gmail.com>
2023-12-08 11:49:44 +0500

    second

--- src/components/Welcomer.jsx
+++ src/components/Welcomer.jsx
@@ -1,12 +1,29 @@
+import React from "react";
+import classes from "./Welcomer.module.css";
+
 const Welcomer = () => {
   return (
-    <section>
-      <header>UX/UI DESIGNER, DEVELOPER</header>
-      <div>data</div>
-      <h1>creative DESICNGER & DEVELOPER</h1>
-      <h2>I AM TTOTOOTOT</h2>
-      <button>contact me</button>
-    </section>
+    <React.Fragment>
+      <div>
+        <header className={classes.header}>
+          <main>UX/UI DESIGNER, DEVELOPER</main>
+          <main>works,about,contact</main>
+        </header>
+
+        <section>
+          <div>data</div>
+          <h1>creative DESICNGER & DEVELOPER</h1>
+          <h2>I AM TTOTOOTOT</h2>
+          <button>contact me</button>
+        </section>
+      </div>
+      <div>
+        <h1>tst</h1>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 export default Welcomer;
